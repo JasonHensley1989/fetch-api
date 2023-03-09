25729ee2f29038b59a7ac73663af6361ddc32a79
Jason Hensley <jasonhensley1989@gmail.com>
2023-03-09 11:38:27 -0600

    basic fetch

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Active");
+fetch('https://reqres.in/api/users/')
+    .then(res => {res.json()
+        if(res.ok){
+            console.log("Success");
+        } else {
+            console.log("Not successful");
+        }
+    })
+    .then(data => console.log(data))
