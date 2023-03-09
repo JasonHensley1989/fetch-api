a141d7de38752c5d8fc85c2c7d1ae4501cfd2394
Jason Hensley <jasonhensley1989@gmail.com>
2023-03-09 11:42:58 -0600

    creating headers, stringify

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-fetch('https://reqres.in/api/users/')
-    .then(res => {res.json()
-        if(res.ok){
-            console.log("Success");
-        } else {
-            console.log("Not successful");
-        }
+// Always set headers and stringify the body
+
+
+fetch('https://reqres.in/api/users/', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: 'User 1',
+
     })
+})
+    .then(res => res.json())
     .then(data => console.log(data))
